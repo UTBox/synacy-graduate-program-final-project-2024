f3dd467ddb3e847993a4de1eccc22c8a941f48b7
seancapulong <sean.capulong@synacy.com>
2024-08-28 21:35:49 +0800

    Update EmployeeService class
    Add employee list bean and initializer method

--- src/main/java/com/synacy/graduate/program/leaveapp/leave_management/employee/EmployeeService.java
+++ src/main/java/com/synacy/graduate/program/leaveapp/leave_management/employee/EmployeeService.java
@@ -7,17 +7,26 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
+    private final List<Employee> employeesList;
 
     @Autowired
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    public EmployeeService(List<Employee> employeesList, EmployeeRepository employeeRepository) {
+        this.employeesList = employeesList;
         this.employeeRepository = employeeRepository;
+        createInitialEmployees();
     }
 
     public Page<Employee> getEmployees(int max, int page) {
         Pageable pageable = PageRequest.of(page - 1, max, Sort.by("id"));
         return employeeRepository.findAll(pageable);
+    }
+
+    private void createInitialEmployees() {
+        employeeRepository.saveAll(employeesList);
     }
 }
