744453c797c78f1ef7a2b8c9a962f3e2b8e7643b
seancapulong <sean.capulong@synacy.com>
2024-08-29 14:34:34 +0800

    Add custom method findAll employees method where isDeleted field is false

--- src/main/java/com/synacy/graduate/program/leaveapp/leave_management/employee/EmployeeRepository.java
+++ src/main/java/com/synacy/graduate/program/leaveapp/leave_management/employee/EmployeeRepository.java
@@ -1,8 +1,11 @@
 package com.synacy.graduate.program.leaveapp.leave_management.employee;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Page<Employee> findAllByIsDeletedIsFalse(Pageable pageable);
 }
