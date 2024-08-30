8aec36e2acd98b8bb792d1013c3b3070607add47
alwyndy <alwyn.dy@synacy.com>
2024-08-31 04:11:59 +0800

    Add controller spec, impl for get all leave applications

--- src/main/java/com/synacy/graduate/program/leaveapp/leave_management/leaveapplication/LeaveApplicationController.java
+++ src/main/java/com/synacy/graduate/program/leaveapp/leave_management/leaveapplication/LeaveApplicationController.java
@@ -2,13 +2,16 @@ package com.synacy.graduate.program.leaveapp.leave_management.leaveapplication;
 
 import com.synacy.graduate.program.leaveapp.leave_management.employee.Employee;
 import com.synacy.graduate.program.leaveapp.leave_management.employee.EmployeeService;
+import com.synacy.graduate.program.leaveapp.leave_management.web.PageResponse;
 import com.synacy.graduate.program.leaveapp.leave_management.web.apierror.ResourceNotFoundException;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class LeaveApplicationController {
@@ -19,6 +22,23 @@ public class LeaveApplicationController {
     public LeaveApplicationController(LeaveApplicationService leaveApplicationService, EmployeeService employeeService) {
         this.leaveApplicationService = leaveApplicationService;
         this.employeeService = employeeService;
+    }
+
+    @GetMapping("api/v1/leave")
+    public PageResponse<ManagerialLeaveApplicationResponse> getLeaveApplications(
+            @RequestParam(name = "max", defaultValue = "2")
+            @Min(value = 1, message = "Max must be greater than 1") Integer max,
+            @RequestParam(name = "page", defaultValue = "1")
+            @Min(value = 1, message = "Page must be greater than 1") Integer page
+    ){
+        Page<LeaveApplication> leaveApplications = leaveApplicationService.getAllLeaveApplications(max, page);
+        long totalCount = leaveApplications.getTotalElements();
+        List<ManagerialLeaveApplicationResponse> leaveApplicationList = leaveApplications
+                .getContent()
+                .stream()
+                .map(ManagerialLeaveApplicationResponse::new)
+                .collect(Collectors.toList());
+        return new PageResponse<>(totalCount, page, leaveApplicationList);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
