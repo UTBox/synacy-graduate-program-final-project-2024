1a39b034716943cdd07d6fc3bb2633e0c6a6a10e
alwyndy <alwyn.dy@synacy.com>
2024-08-31 04:51:34 +0800

    Add service impl, spec for get leaves by manager

--- src/main/java/com/synacy/graduate/program/leaveapp/leave_management/leaveapplication/LeaveApplicationService.java
+++ src/main/java/com/synacy/graduate/program/leaveapp/leave_management/leaveapplication/LeaveApplicationService.java
@@ -2,8 +2,13 @@ package com.synacy.graduate.program.leaveapp.leave_management.leaveapplication;
 
 import com.synacy.graduate.program.leaveapp.leave_management.employee.Employee;
 import com.synacy.graduate.program.leaveapp.leave_management.employee.EmployeeRepository;
+import com.synacy.graduate.program.leaveapp.leave_management.employee.EmployeeService;
 import com.synacy.graduate.program.leaveapp.leave_management.web.apierror.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
@@ -13,10 +18,18 @@ import java.time.LocalDate;
 public class LeaveApplicationService {
 
     private final LeaveApplicationRepository leaveApplicationRepository;
+    private final EmployeeService employeeService;
 
     @Autowired
-    public LeaveApplicationService(LeaveApplicationRepository leaveApplicationRepository) {
+    public LeaveApplicationService(LeaveApplicationRepository leaveApplicationRepository, EmployeeService employeeService) {
         this.leaveApplicationRepository = leaveApplicationRepository;
+        this.employeeService = employeeService;
+    }
+
+    Page<LeaveApplication> getLeavesByManager(int max, int page, Long managerId){
+        Pageable pageable = PageRequest.of(page - 1, max, Sort.by("id"));
+        Employee manager = employeeService.getEmployeeById(managerId).get();
+        return leaveApplicationRepository.findAllByManager(manager, pageable);
     }
 
     LeaveApplication createLeaveApplication(Employee employee, CreateLeaveApplicationRequest createLeaveApplicationRequest) {
