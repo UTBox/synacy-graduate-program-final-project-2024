338b6e48db81e0bf713ce7a0b72e48974d0c0bdb
alwyndy <alwyn.dy@synacy.com>
2024-08-31 04:51:47 +0800

    Add controller impl, spec for get leaves by manager

--- src/main/java/com/synacy/graduate/program/leaveapp/leave_management/leaveapplication/LeaveApplicationController.java
+++ src/main/java/com/synacy/graduate/program/leaveapp/leave_management/leaveapplication/LeaveApplicationController.java
@@ -1,14 +1,20 @@
 package com.synacy.graduate.program.leaveapp.leave_management.leaveapplication;
 
 import com.synacy.graduate.program.leaveapp.leave_management.employee.Employee;
-import com.synacy.graduate.program.leaveapp.leave_management.employee.EmployeeService;
+import com.synacy.graduate.program.leaveapp.leave_management.web.PageResponse;
 import com.synacy.graduate.program.leaveapp.leave_management.web.apierror.ResourceNotFoundException;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import org.springframework.data.domain.Page;
+import com.synacy.graduate.program.leaveapp.leave_management.employee.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class LeaveApplicationController {
@@ -19,6 +25,25 @@ public class LeaveApplicationController {
     public LeaveApplicationController(LeaveApplicationService leaveApplicationService, EmployeeService employeeService) {
         this.leaveApplicationService = leaveApplicationService;
         this.employeeService = employeeService;
+    }
+
+    @GetMapping("/api/v1/leave/manager/{id}")
+    public PageResponse<ManagerialLeaveApplicationResponse> getLeavesByManager(
+            @RequestParam(name = "max", defaultValue = "2")
+            @Min(value = 1, message = "Max must be greater than 1") Integer max,
+            @RequestParam(name = "page", defaultValue = "1")
+            @Min(value = 1, message = "Page must be greater than 1") Integer page,
+            @PathVariable(name = "id") Long managerId
+    ){
+        Page<LeaveApplication> leaveApplications = leaveApplicationService.getLeavesByManager(max, page, managerId);
+        long count = leaveApplications.getTotalElements();
+        List<ManagerialLeaveApplicationResponse> leaveApplicationResponseList = leaveApplications
+                .getContent()
+                .stream()
+                .map(ManagerialLeaveApplicationResponse::new)
+                .collect(Collectors.toList());
+
+        return new PageResponse<>(count, page, leaveApplicationResponseList);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
---
+++ src/test/groovy/com/synacy/graduate/program/leaveapp/leave_management/leaveapplication/LeaveApplicationControllerSpec.groovy
@@ -0,0 +1,117 @@
+package com.synacy.graduate.program.leaveapp.leave_management.leaveapplication
+
+import com.synacy.graduate.program.leaveapp.leave_management.employee.Employee
+import com.synacy.graduate.program.leaveapp.leave_management.employee.EmployeeService
+import com.synacy.graduate.program.leaveapp.leave_management.web.PageResponse
+import org.springframework.data.domain.Page
+import spock.lang.Specification
+
+import java.time.LocalDate
+
+
+class LeaveApplicationControllerSpec extends Specification {
+
+    LeaveApplicationController leaveApplicationController
+    LeaveApplicationService leaveApplicationService = Mock()
+    EmployeeService employeeService = Mock()
+
+    def setup(){
+        leaveApplicationController = new LeaveApplicationController(leaveApplicationService, employeeService)
+    }
+
+    def "getLeavesByManager should return a paginated leave applications of all employees under the direct supervision of the given manager"(){
+        given:
+        int max = 10
+        int page = 1
+        Long managerIdFilter = 1
+        int totalCount = 2
+
+        Long managerId = 1
+        String managerFirstName = "Man"
+        String managerLastName = "Doe"
+
+        Long id1 = 1
+        String employeeFirstName1 = "John"
+        String employeeLastName1 = "Doe"
+        String employeeName1 = "John Doe"
+        LocalDate startDate1 = LocalDate.now()
+        LocalDate endDate1 = LocalDate.now()
+        int workDays1 = 1
+        String reason1 = "Reason 1"
+        LeaveApplicationStatus status1 = LeaveApplicationStatus.APPROVED
+
+        Long id2 = 2
+        String employeeFirstName2 = "Johny"
+        String employeeLastName2 = "Doey"
+        String employeeName2 = "Johny Doey"
+        LocalDate startDate2 = LocalDate.now()
+        LocalDate endDate2 = LocalDate.now()
+        int workDays2 = 1
+        String reason2 = "Reason 1"
+        LeaveApplicationStatus status2 = LeaveApplicationStatus.APPROVED
+
+        Employee employee1 = Mock(Employee){
+            firstName >> employeeFirstName1
+            lastName >> employeeLastName1
+        }
+        Employee employee2 = Mock(Employee){
+            firstName >> employeeFirstName2
+            lastName >> employeeLastName2
+        }
+        Employee managerObject = Mock(Employee){
+            id >> managerId
+            firstName >> managerFirstName
+            lastName >> managerLastName
+        }
+
+        LeaveApplication leaveApplication1 = Mock() {
+            id >> id1
+            employee >> employee1
+            manager >> managerObject
+            startDate >> startDate1
+            endDate >> endDate1
+            workDays >> workDays1
+            reason >> reason1
+            status >> status1
+        }
+
+        LeaveApplication leaveApplication2 = Mock() {
+            id >> id2
+            employee >> employee2
+            manager >> managerObject
+            startDate >> startDate2
+            endDate >> endDate2
+            workDays >> workDays2
+            reason >> reason2
+            status >> status2
+        }
+
+        List<LeaveApplication> leaveApplicationList = [leaveApplication1, leaveApplication2]
+        Page<LeaveApplication> leaveApplicationPage = Mock() {
+            content >> leaveApplicationList
+            totalElements >> totalCount
+        }
+
+        when:
+        PageResponse<ManagerialLeaveApplicationResponse> response = leaveApplicationController.getLeavesByManager(max, page, managerIdFilter)
+
+        then:
+        1 * leaveApplicationService.getLeavesByManager(max, page, managerIdFilter) >> leaveApplicationPage
+        totalCount == response.totalCount()
+        page == response.pageNumber()
+
+        employeeName1 == response.content()[0].getEmployeeName()
+        startDate1 == response.content()[0].getStartDate()
+        endDate1 == response.content()[0].getEndDate()
+        workDays1 == response.content()[0].getWorkDays()
+        reason1 == response.content()[0].getReason()
+        status1 == response.content()[0].getStatus()
+
+        employeeName2 == response.content()[1].getEmployeeName()
+        startDate2 == response.content()[1].getStartDate()
+        endDate2 == response.content()[1].getEndDate()
+        workDays2 == response.content()[1].getWorkDays()
+        reason2 == response.content()[1].getReason()
+        status2 == response.content()[1].getStatus()
+    }
+}
