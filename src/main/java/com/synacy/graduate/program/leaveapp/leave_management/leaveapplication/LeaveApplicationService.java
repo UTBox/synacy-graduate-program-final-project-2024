03c92a2deac3ae19f850cebb0e949426859a4068
seancapulong <sean.capulong@synacy.com>
2024-08-31 12:59:02 +0800

    Add updateLeaveApplication method and implementation to handle approved/rejected leave requests

--- src/main/java/com/synacy/graduate/program/leaveapp/leave_management/leaveapplication/LeaveApplicationService.java
+++ src/main/java/com/synacy/graduate/program/leaveapp/leave_management/leaveapplication/LeaveApplicationService.java
@@ -1,7 +1,6 @@
 package com.synacy.graduate.program.leaveapp.leave_management.leaveapplication;
 
 import com.synacy.graduate.program.leaveapp.leave_management.employee.Employee;
-import com.synacy.graduate.program.leaveapp.leave_management.employee.EmployeeRepository;
 import com.synacy.graduate.program.leaveapp.leave_management.employee.EmployeeRole;
 import com.synacy.graduate.program.leaveapp.leave_management.employee.EmployeeService;
 import com.synacy.graduate.program.leaveapp.leave_management.web.apierror.ResourceNotFoundException;
@@ -11,28 +10,35 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class LeaveApplicationService {
 
     private final LeaveApplicationRepository leaveApplicationRepository;
     private final EmployeeService employeeService;
+    private final LeaveQuantityModifier leaveQuantityModifier;
 
     @Autowired
-    public LeaveApplicationService(LeaveApplicationRepository leaveApplicationRepository, EmployeeService employeeService) {
+    public LeaveApplicationService(
+            LeaveApplicationRepository leaveApplicationRepository,
+            EmployeeService employeeService,
+            LeaveQuantityModifier leaveQuantityModifier) {
         this.leaveApplicationRepository = leaveApplicationRepository;
         this.employeeService = employeeService;
+        this.leaveQuantityModifier = leaveQuantityModifier;
     }
 
-    Page<LeaveApplication> getLeavesByManager(int max, int page, Long managerId){
+    Page<LeaveApplication> getLeavesByManager(int max, int page, Long managerId) {
         Pageable pageable = PageRequest.of(page - 1, max, Sort.by("id"));
         Employee manager = employeeService.getEmployeeById(managerId)
                 .orElseThrow(ResourceNotFoundException::new);
 
-        if(manager.getRole() != EmployeeRole.MANAGER){
+        if (manager.getRole() != EmployeeRole.MANAGER) {
             throw new NotAManagerException();
         }
 
@@ -72,7 +78,7 @@ public class LeaveApplicationService {
         Integer leaveWorkDays = 0;
         LocalDate currentDate = startDate;
 
-        while(!currentDate.isAfter(endDate)) {
+        while (!currentDate.isAfter(endDate)) {
             if (currentDate.getDayOfWeek() != DayOfWeek.SATURDAY && currentDate.getDayOfWeek() != DayOfWeek.SUNDAY) {
                 leaveWorkDays++;
             }
@@ -80,5 +86,38 @@ public class LeaveApplicationService {
         }
 
         return leaveWorkDays;
+    }
+
+    @Transactional
+    LeaveApplication updateLeaveApplication(LeaveApplication leave, UpdateLeaveApplicationRequest request) {
+        if (leave.getStatus() != LeaveApplicationStatus.PENDING) {
+            throw new InvalidLeaveApplicationStatusException("Leave application status is not PENDING.");
+        }
+
+        if (request.getLeaveApplicationStatus() == LeaveApplicationStatus.APPROVED
+                || request.getLeaveApplicationStatus() == LeaveApplicationStatus.REJECTED) {
+            leave.setStatus(request.getLeaveApplicationStatus());
+            updateLeaveQuantityBasedOnStatus(leave);
+        }
+
+        return leaveApplicationRepository.save(leave);
+    }
+
+    Optional<LeaveApplication> getLeaveApplicationById(Long id) {
+        return leaveApplicationRepository.findById(id);
+    }
+
+    private void updateLeaveQuantityBasedOnStatus(LeaveApplication leave) {
+        switch (leave.getStatus()) {
+            case APPROVED:
+                leaveQuantityModifier.deductLeaveQuantityBasedOnApprovedRequest(leave);
+                break;
+            case REJECTED:
+            case CANCELLED:
+                leaveQuantityModifier.addLeaveQuantityBasedOnRejectedOrCancelledRequest(leave);
+                break;
+            default:
+                break;
+        }
     }
 }
