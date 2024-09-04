00b3590450588737fe943a2dce622fc698b10f2f
alwyndy <alwyn.dy@synacy.com>
2024-09-05 05:48:51 +0800

    Add role in response

--- src/main/java/com/synacy/graduate/program/leaveapp/leave_management/employee/ManagerResponse.java
+++ src/main/java/com/synacy/graduate/program/leaveapp/leave_management/employee/ManagerResponse.java
@@ -7,10 +7,12 @@ public class ManagerResponse {
     private Long id;
     private String firstName;
     private String lastName;
+    private EmployeeRole role;
 
     ManagerResponse(Employee manager){
         this.id = manager.getId();
         this.firstName = manager.getFirstName();
         this.lastName = manager.getLastName();
+        this.role = manager.getRole();
     }
 }
