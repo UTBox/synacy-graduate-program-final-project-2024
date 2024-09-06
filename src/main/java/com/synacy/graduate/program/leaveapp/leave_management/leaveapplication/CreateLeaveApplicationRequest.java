e1ab4613a8e84d887164887be3eafb914f48e711
seancapulong <sean.capulong@synacy.com>
2024-09-06 11:47:23 +0800

    Add @NotNull annotations to create leave application request

--- src/main/java/com/synacy/graduate/program/leaveapp/leave_management/leaveapplication/CreateLeaveApplicationRequest.java
+++ src/main/java/com/synacy/graduate/program/leaveapp/leave_management/leaveapplication/CreateLeaveApplicationRequest.java
@@ -1,13 +1,21 @@
 package com.synacy.graduate.program.leaveapp.leave_management.leaveapplication;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Getter
 public class CreateLeaveApplicationRequest {
+    @NotNull(message = "Employee id is null")
     private Long employeeId;
+
+    @NotNull(message = "Start date is null")
     private LocalDate startDate;
+
+    @NotNull(message = "End date is null")
     private LocalDate endDate;
+
+    @NotNull(message = "Reason is null")
     private String reason;
 }
